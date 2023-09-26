7101b4f3037fe8ce0df1d58d5088e449494ad0f6
nandobfer <nandobfer@yahoo.com.br>
2023-09-26 15:22:14 -0300

    feat: working auto login

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,7 +2,6 @@ import { app, shell, BrowserWindow } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
-import AutoLaunch from 'electron-auto-launch'
 
 function createWindow(): void {
   // Create the browser window.
@@ -36,7 +35,7 @@ function createWindow(): void {
   // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
-} else {
+  } else {
     // mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
     mainWindow.loadURL('https://app.agenciaboz.com.br')
   }
@@ -46,15 +45,6 @@ function createWindow(): void {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  const autoLauncher = new AutoLaunch({
-    name: 'Boz',
-    path: app.getPath('exe')
-  })
-
-  autoLauncher.isEnabled().then((isEnabled) => {
-    if (!isEnabled) autoLauncher.enable()
-  })
-
   // Set app user model id for windows
   electronApp.setAppUserModelId('br.com.agenciaboz.app')
 
@@ -85,3 +75,7 @@ app.on('window-all-closed', () => {
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
+
+app.setLoginItemSettings({
+  openAtLogin: true
+})
